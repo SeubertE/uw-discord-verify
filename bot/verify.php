ee4338134476246f67832e3d425a254a168b5835
Eugene Seubert <SeubertE@users.noreply.github.com>
2021-05-03 12:10:10 -0700

    ahhhhhhhhhhhhhhhhhhhhhhhhh

--- bot/verify.php
+++ bot/verify.php
@@ -31,42 +31,61 @@ if(get('code')) {
 }
 
 if(session('access_token')) {
-    //get userid
-    $user = callApi('https://discord.com/api/users/@me', '', array('Authorization: Bearer ' . session('access_token'), 'Content-Type' => 'application/json'));
+  //get userid
+  $user = callApi('https://discord.com/api/users/@me', '', array('Authorization: Bearer ' . session('access_token'), 'Content-Type' => 'application/json'));
 
-    //http header to auth
-    $header = array(
-        'Content-Type: application/json',
-        'Authorization: Bot ' . $botToken,
-    );
+  //http header to auth
+  $header = array(
+      'Content-Type: application/json',
+      'Authorization: Bot ' . $botToken,
+  );
 
-    //send message to channel that user was verified
-    $postdata = array(
-      //"content" => 'User ID: ' . $user->id . ' verified with UW NETID ' . $_SERVER['REMOTE_USER'],
+  // i have lost my sanity and now trying to copy paste some code to get this to work
+  $curl = curl_init();
+
+  curl_setopt($curl, CURLOPT_URL, 'https://discordapp.com/api/channels/'.$channel_id.'/messages');
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+      // 'Content-Type: application/json',
+      'Authorization: Bot '. $botToken
+  ]);
+  curl_setopt($curl, CURLOPT_POST, true);
+
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode([
+      'content' => 'test'
+  ]));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $server_output = curl_exec($curl);
+  curl_close ($curl);
+  print_r(json_decode($server_output, true));
+
+  //send message to channel that user was verified
+  $postdata = array(
+    //"content" => 'User ID: ' . $user->id . ' verified with UW NETID ' . $_SERVER['REMOTE_USER'],
+    "content" => 'test'
+  );
+  $rvalsendmessage = callApi('https://discord.com/api/channels/' . $channelId . '/messages', 'POST', $header, $postdata);
+
+  //add them to the server!
+  $putdata = array(
       "access_token" => session('access_token'),
-      "content" =>  'test'
-    );
-    $rvalsendmessage = callApi('https://discord.com/api/channels/' . $channelId . '/messages', 'POST', $header, $postdata);
+      "nick" => $_SERVER['REMOTE_USER'],
+      "roles" => array($role)
+  );
 
-    //add them to the server!
-    $putdata = array(
-        "access_token" => session('access_token'),
-        "nick" => $_SERVER['REMOTE_USER'],
-        "roles" => array($role)
-    );
+  $rvaladdtoguild = callApi('https://discord.com/api/guilds/' . $guildId . '/members/' . $user->id, 'PUT', $header, $putdata);
 
-    $rvaladdtoguild = callApi('https://discord.com/api/guilds/' . $guildId . '/members/' . $user->id, 'PUT', $header, $putdata);
+  //change nick name and role (if already in server)
+  $rvalchangeandverify = callApi('https://discord.com/api/guilds/' . $guildId . '/members/' . $user->id, 'PATCH', $header, $putdata);
 
-    //change nick name and role (if already in server)
-    $rvalchangeandverify = callApi('https://discord.com/api/guilds/' . $guildId . '/members/' . $user->id, 'PATCH', $header, $putdata);
+  //redirect after done
+  //header('Location: ' . $postauthURL);
 
-    //redirect after done
-    //header('Location: ' . $postauthURL);
+  //debugging
+  print_r($postdata)
+  print_r($rvalsendmessage);
 
-    //debugging
-    print_r($rvalsendmessage);
-
-    die();
+  die();
 } 
 
 if(get('action')=='acceptedterms'){
@@ -112,8 +131,8 @@ function callApi($url, $method = '', $headers = array(), $data = array()) {
     switch ($method)
     {
         case "POST":
-          //curl_setopt($curl, CURLOPT_POST, true);
-          curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+          curl_setopt($curl, CURLOPT_POST, true);
+          //curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
           curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
         case "PUT":
           curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
